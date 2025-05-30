10a0daa0a2134f8b18882defbd8700a105e4c38a
pshreya747 <pshreya0711@gmail.com>
2025-05-30 23:12:12 +0530

    Implemented lexical and home page.

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,32 +1,7 @@
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
 import React from "react";
 
 const Document = () => {
-  return (
-    <div>
-      <Header>
-        <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title">This is a test document</p>
-        </div>
-        <SignedOut>
-          <SignInButton />
-          {/* <SignUpButton /> */}
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </Header>
-      <Editor />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Document;
